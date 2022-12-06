96a38b83b81d8fa622a32a63f721f5d52eabc29f
nishi719 <106315974+nishi719@users.noreply.github.com>
2022-12-06 14:19:47 +0530

    Add files via upload

--- Project/urls.py
+++ Project/urls.py
@@ -1,10 +1,30 @@
-from django.urls import path
-from .views import HomePageView, PostDetailView, AddPostView
+"""mysite URL Configuration
 
-app_name = 'feed'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from feed import urls as feed_urls
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='index'),
-    path('detail/<int:pk>/', PostDetailView.as_view(), name='detail'),
-    path('post/', AddPostView.as_view(), name='post'),
+    path('admin/', admin.site.urls),
+    path('', include(feed_urls, namespace='feed')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
